83d89aa3e8e5325b309c3d7c8cc0450f4a4abc44
Kirill Tsaryov <71272612+kdtsaryov@users.noreply.github.com>
2022-05-14 18:01:15 +0300

    Add Authorization

--- Код/SharingSkills_HSE_backend/SharingSkills_HSE_backend/Startup.cs
+++ Код/SharingSkills_HSE_backend/SharingSkills_HSE_backend/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SharingSkills_HSE_backend.Models;
 using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SharingSkills_HSE_backend.Other;
 
 namespace SharingSkills_HSE_backend
 {
@@ -19,17 +22,17 @@ namespace SharingSkills_HSE_backend
         public IConfiguration Configuration { get; }
 
         /// <summary>
-        /// ����������� ��������
+        /// Подключение сервисов
         /// </summary>
-        /// <param name="services">�������</param>
+        /// <param name="services">Сервисы</param>
         public void ConfigureServices(IServiceCollection services)
         {
-            // ����������� JSON �������������
+            // Подключение JSON сериализатора
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-            // ����������� ������������
+            // Подключение контроллеров
             services.AddControllers();
-            // ����������� ��������� ���� ������
+            // Подключение контекста базы данных
             if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
                 services.AddDbContext<SharingSkillsContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("SharingSkillsContextProd")));
@@ -37,6 +40,31 @@ namespace SharingSkills_HSE_backend
                 services.AddDbContext<SharingSkillsContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("SharingSkillsContext")));
             services.BuildServiceProvider().GetService<SharingSkillsContext>().Database.Migrate();
+            // Подключение аутентификации
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            // Укзывает, будет ли валидироваться издатель при валидации токена
+                            ValidateIssuer = true,
+                            // Строка, представляющая издателя
+                            ValidIssuer = AuthOptions.ISSUER,
+
+                            // Будет ли валидироваться потребитель токена
+                            ValidateAudience = true,
+                            // Установка потребителя токена
+                            ValidAudience = AuthOptions.AUDIENCE,
+                            // Будет ли валидироваться время существования
+                            ValidateLifetime = true,
+
+                            // Установка ключа безопасности
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                            // Валидация ключа безопасности
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,14 +74,18 @@ namespace SharingSkills_HSE_backend
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
