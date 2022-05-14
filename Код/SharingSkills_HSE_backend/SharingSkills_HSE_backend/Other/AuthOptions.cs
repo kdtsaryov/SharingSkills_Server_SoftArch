85abd58e491a2add4755e4970b9bebdbe2c46f11
Kirill Tsaryov <71272612+kdtsaryov@users.noreply.github.com>
2022-05-14 18:00:07 +0300

    Add AuthOptions

---
+++ Код/SharingSkills_HSE_backend/SharingSkills_HSE_backend/Other/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SharingSkills_HSE_backend.Other
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // Издатель токена
+        public const string AUDIENCE = "MyAuthClient"; // Потребитель токена
+        const string KEY = "mysupersecret_secretkey!123";   // Ключ для шифрования
+        public const int LIFETIME = 1; // Время жизни токена - 1 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
